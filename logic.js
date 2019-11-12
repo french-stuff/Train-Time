7ac3b9ed1b76e219e877db8754fd9a3c1390d28b
Franchesca <legarreta.french@gmail.com>
2019-11-12 12:01:54 -0500

    added firebaseconfig

---
+++ logic.js
@@ -0,0 +1,11 @@
+//
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCN01k3Tb9yCBnbJAy5zloiKYlaV-sBVrU",
+    authDomain: "argon-bbc2e.firebaseapp.com",
+    databaseURL: "https://argon-bbc2e.firebaseio.com",
+    projectId: "argon-bbc2e",
+    storageBucket: "argon-bbc2e.appspot.com",
+    messagingSenderId: "719768225094",
+    appId: "1:719768225094:web:6dbef24628269fcb871147"
+  };
